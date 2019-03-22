1be671d0d4209724bba91c5c25f026a669fa0f10
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-03-21 22:58:14 -0700

    create Ilios template lint plugin

---
+++ index.js
@@ -0,0 +1,30 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = {
+  name: 'ilios',
+
+  rules: {
+
+  },
+
+  configurations: {
+    recommended: {
+      extends: 'recommended',
+      rules: {
+        'img-alt-attributes': true,
+        'inline-link-to': true,
+        'deprecated-each-syntax': true,
+        'deprecated-inline-view-helper': true,
+        'eol-last': true,
+        'link-href-attributes': true,
+        'no-bare-strings': true,
+        'no-extra-mut-helper-argument': true,
+        'no-implicit-this': true,
+        'no-trailing-dot-in-path-expression': true,
+        'no-trailing-spaces': true,
+        quotes: 'double',
+      }
+    }
+  }
+};
